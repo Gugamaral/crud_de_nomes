9aee9f58c26d09950cf4ee9f8bc05a5e993fbb83
Gustavo Amaral <gugamaral@gmail.com>
2024-05-24 21:45:43 -0300

    Versão 1.1

--- main.py
+++ main.py
@@ -30,3 +30,71 @@ for cadastro in cadastros:
     print(cadastro)
 
 
+# lista de frutas
+frutas =  ['Maracujá', 'Banana', 'Maçã']
+
+# usiario informa o nome da nova fruta
+nova_fruta = input('Informe o nome da nova fruta: ')
+
+# nova fruta é inserida na lista
+frutas.append(nova_fruta)
+
+# exibe na tela nova lista
+for fruta in frutas:
+    print(fruta)
+
+
+------------------------
+
+
+cidades = ['Brasília', 'Taguatinga', 'Gama', 'Sobradinho']
+
+cidade_deletada = input('Nome da cidade a ser deletada: ')
+
+try:
+    cidades.remove(cidade_deletada)
+except:
+    print("Não foi possível remover a cidade. ")
+
+for cidade in cidades:
+    print(cidade)
+
+-----------------------------
+
+nomes = ['Adão', "Helião", "Chups", 'Carminha', 'Maria Baixinha', 'João Boca Azeda', 'Beltiza', 'Erondina', 'Juninho Lobrega', 'Orizonmarden', 'Zequinha', 'Ninoca', 'Joabe', 'Noemia', 'Rosa', 'Juscelino']
+
+# usuário informa o nome que deseja pesquisar
+nome = input('Digite o nome a ser pesquisado: ').capitalize()
+
+# retorna o índice do nome pesquisado
+
+# verifica se o nome está na lista ou não
+try:
+    indice = nomes.index(nome)
+    print(f'Nome encontrado: {nome} no índice {indice}.')
+except:
+    print(f'{nome} não encontrado na lista.')
+
+------------------------------------------
+
+# importa biblioteca
+import os
+
+tarefas = []
+
+while True:
+    tarefas.sort()
+    nova_tarefa = input('Informe suas tarefas do dia ou deixe vazio para encerrar e exibir a lista: ')
+
+    if nova_tarefa != '':
+        tarefas.append(nova_tarefa)
+        continue
+    else:
+        break
+    
+os.system("cls")
+
+print(f'{'-'*35}LISTA DE TAREFAS DO DIA{'-'*35}')
+
+for tarefa in tarefas:
+    print(tarefa)
